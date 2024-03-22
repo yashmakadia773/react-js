4420007e733bfa6ad1d3974822d726acf659b3e9
yashmakadia773 <133627880+yashmakadia773@users.noreply.github.com>
2024-03-22 16:36:30 +0530

    Add files via upload

---
+++ book/src/App.js
@@ -0,0 +1,8 @@
+import Book from "./Component/Book";
+function App() {
+  return (
+     <Book/>
+  );
+}
+
+export default App;
---
+++ book/src/Component/Book.js
@@ -0,0 +1,7 @@
+const Book=[
+    {
+        path:'/',
+        name:'the name of the wind',
+        img:'https://m.media-amazon.com/images/I/611iKJa7a-L._AC_UF1000,1000_QL80_.jpg'
+    }
+]
